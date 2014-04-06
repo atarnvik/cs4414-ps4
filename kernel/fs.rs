2eae00b80afdadd3807669b2cf3a0e21580d02e6
Matt Bloom <mattbloom52@ubuntu.(none)>
2014-04-06 19:49:17 -0400

    simplified code using imports

--- kernel/fs.rs
+++ kernel/fs.rs
@@ -5,18 +5,21 @@ use core::option::{Some, Option, None}; // Match statement
 use core::iter::Iterator;
 use kernel::vec::Vec;
 use super::super::platform::*;
-use kernel::sgash;
+use kernel::sgash::cstr;
+use kernel::sgash::drawcstr;
+use kernel::sgash::putcstr;
 use core::iter::Iterator;
+use core::slice::iter;
 
 pub struct DirNode {
-    name: sgash::cstr,
+    name: cstr,
     parent: *mut DirNode,
     dchildren: *mut Vec<*mut DirNode>,
     fchildren : *mut Vec<*mut FileNode>,
 }
  
 impl DirNode {
-    pub unsafe fn new(file_name: sgash::cstr, dad: *mut DirNode) -> DirNode {
+    pub unsafe fn new(file_name: cstr, dad: *mut DirNode) -> DirNode {
         let this = DirNode {
             name: file_name,
             parent: dad,
@@ -27,43 +30,43 @@ impl DirNode {
         this
     }
 
-    pub unsafe fn list_directory (&mut self) -> Vec<sgash::cstr>{
-        let mut ls = &mut Vec::new() as *mut Vec<*mut sgash::cstr>;
-        for d in iter((*self.dchildren).as_mut_slice()){
-        	ls.push(d);
+    pub unsafe fn list_directory (&mut self){
+        for d in iter((self.dchildren).as_mut_slice()){
+        	putcstr(d.name);
+        	drawcstr(d.name);
         }
 
     }
  
-    pub unsafe fn create_directory(&mut self, d : sgash::cstr) {
+    pub unsafe fn create_directory(&mut self, d : cstr) {
         let newNode : *mut DirNode = &mut DirNode::new(d, self); 
        (*self.dchildren).push(newNode);
     }
 
-    pub unsafe fn create_file(&mut self, d : sgash::cstr) {
-    	let empty = sgash::cstr::new(0);
+    pub unsafe fn create_file(&mut self, d : cstr) {
+    	let empty = cstr::new(0);
         let newNode : *mut FileNode = &mut FileNode::new(d, self, empty); 
        (*self.fchildren).push(newNode);
     }
 
-    pub unsafe fn write_file(&mut self, d : sgash::cstr, contents : sgash::cstr) {
+    pub unsafe fn write_file(&mut self, d : cstr, contents : cstr) {
         let newNode : *mut FileNode = &mut FileNode::new(d, self, contents); 
        (*self.fchildren).push(newNode);
     }
 
-    // pub unsafe fn get_file(&mut self, name : sgash::cstr){
+    // pub unsafe fn get_file(&mut self, name : cstr){
 
     // }
 }
  
 pub struct FileNode {
-    name: sgash::cstr,
+    name: cstr,
     parent: *mut DirNode,
-    contents: sgash::cstr,
+    contents: cstr,
 }
 
 impl FileNode {
-    pub unsafe fn new (file_name: sgash::cstr, dad: *mut DirNode, words: sgash::cstr) -> FileNode {
+    pub unsafe fn new (file_name: cstr, dad: *mut DirNode, words: cstr) -> FileNode {
         let this = FileNode {
             name: file_name,
             parent: dad,
@@ -72,12 +75,12 @@ impl FileNode {
         this
     }
 
-    pub unsafe fn getName(&self) -> sgash::cstr {
+    pub unsafe fn getName(&self) -> cstr {
         self.name
     }
 
     //cat <file>
-    pub fn read_file(&self) -> sgash::cstr {
+    pub fn read_file(&self) -> cstr {
         self.contents
     }
 }
